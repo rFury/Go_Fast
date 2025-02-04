1ae569610581be52ddc065aa5425f104ba28817c
rfury <youssefwerfellicpm@gmail.com>
2025-02-04 12:49:45 +0100

    Signin/up HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+];
