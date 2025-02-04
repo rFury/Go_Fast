1e22cd62cba388903c2b1c5b82f136b2109f86b5
rfury <youssefwerfellicpm@gmail.com>
2025-02-04 12:29:24 +0100

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'go-fast',
+  webDir: 'dist/go-fast/browser'
+};
+
+export default config;
