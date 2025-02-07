c1a9ef8b8e642bba0a2d1cb420ff7f60c7d6bd28
rFury <youssefwerfellicpm@gmail.com>
2025-02-07 01:36:35 +0100

    mini projet done

--- src/app/mini-projet/Service/main.service.ts
+++ src/app/mini-projet/Service/main.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+export interface Task {
+  _id?: string;
+  taskName: string;
+  taskDate: string;
+  taskDuration: number;
+  importance: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
+  private apiUrl = "http://127.0.0.1:3000/tasks";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTaskById(id: string): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(id: string, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
+  }
+
+  deleteTask(id: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.apiUrl}/${id}`);
+  }
 }
