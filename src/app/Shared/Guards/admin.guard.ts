1b02ed580abdb04e5c408963776115745cf8da50
rfury <youssefwerfellicpm@gmail.com>
2025-05-15 13:30:29 +0100

    guards fixed

--- src/app/Shared/Guards/admin.guard.ts
+++ src/app/Shared/Guards/admin.guard.ts
@@ -6,10 +6,25 @@ import { SuperAuthService } from '../Services/super-auth-service.service';
 export const adminGuard: CanActivateFn = (route, state): boolean | UrlTree => {
   const _authService = inject(SuperAuthService);
   const router = inject(Router);
+  const who = _authService.decodeToken().type;
   
     if( _authService.isLoggedIn()){
-      return (_authService.decodeToken().type === 'user' || _authService.decodeToken().type === 'super')?true:false;
+      if (who === 'user' || who === 'super'){
+        return true;
+      }else{
+        if(who === 'agent'){
+          return router.createUrlTree(['admin/agents/'], {
+            queryParams: { returnUrl: state.url },
+          });
+        }else{
+          return router.createUrlTree(['/'], {
+            queryParams: { returnUrl: state.url },
+          });
+        }
+      }
     }else{
-    return false;
+      return router.createUrlTree(['admin/sign-in'], {
+        queryParams: { returnUrl: state.url },
+      });
   }
 };
--- src/app/Shared/Guards/agent.guard.ts
+++ src/app/Shared/Guards/agent.guard.ts
@@ -5,8 +5,22 @@ import { SuperAuthService } from '../Services/super-auth-service.service';
 
 export const agentGuard: CanActivateFn = (route, state): boolean | UrlTree => {
   const _authService = inject(SuperAuthService);
+  const _router = inject(Router);
+  const who = _authService.decodeToken().type;
     if(_authService.isLoggedIn()){
-      return (_authService.decodeToken().type === 'agent')?true:false;
+      if (who === 'agent'){
+        return true;
+      }else{
+        if(who === 'client'){
+          return _router.createUrlTree(['/'], {
+            queryParams: { returnUrl: state.url },
+          });
+        }else{
+          return _router.createUrlTree(['admin/dashboard/'], {
+            queryParams: { returnUrl: state.url },
+          });
+        }
+      }
     }else{
     return false;
   }
